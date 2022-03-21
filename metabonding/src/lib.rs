bf7a4244edc4b9cae68ed29fefda21132731dd32
dorin.iancu <dorin.iancu@elrond.com>
2022-03-21 13:06:11 +0200

    opt set first week start epoch

--- metabonding/src/lib.rs
+++ metabonding/src/lib.rs
@@ -20,12 +20,20 @@ pub trait Metabonding:
     + util::UtilModule
 {
     #[init]
-    fn init(&self, signer: ManagedAddress) {
+    fn init(
+        &self,
+        signer: ManagedAddress,
+        #[var_args] opt_first_week_start_epoch: OptionalValue<u64>,
+    ) {
         self.signer().set(&signer);
         self.set_paused(true);
 
-        let current_epoch = self.blockchain().get_block_epoch();
-        self.first_week_start_epoch().set_if_empty(&current_epoch);
+        let first_week_start_epoch = match opt_first_week_start_epoch {
+            OptionalValue::Some(epoch) => epoch,
+            OptionalValue::None => self.blockchain().get_block_epoch(),
+        };
+        self.first_week_start_epoch()
+            .set_if_empty(&first_week_start_epoch);
     }
 
     #[only_owner]
