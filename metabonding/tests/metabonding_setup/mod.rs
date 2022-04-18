985cc5e55ef73e42a51dd81ab25e59902b8b017a
dorin.iancu <dorin.iancu@elrond.com>
2022-04-18 16:35:59 +0300

    claim multiple weeks

--- metabonding/tests/metabonding_setup/mod.rs
+++ metabonding/tests/metabonding_setup/mod.rs
@@ -1,5 +1,7 @@
 use elrond_wasm::{
-    api::ED25519_SIGNATURE_BYTE_LEN, elrond_codec::multi_types::OptionalValue, types::Address,
+    api::ED25519_SIGNATURE_BYTE_LEN,
+    elrond_codec::multi_types::OptionalValue,
+    types::{Address, MultiValueEncoded},
 };
 use elrond_wasm_debug::{
     managed_address, managed_biguint, managed_buffer, managed_token_id, rust_biguint,
@@ -345,12 +347,18 @@ where
     ) -> TxResult {
         self.b_mock
             .execute_tx(caller, &self.mb_wrapper, &rust_biguint!(0), |sc| {
-                sc.claim_rewards(
-                    week,
-                    managed_biguint!(user_delegation_supply),
-                    managed_biguint!(user_lkmex_staked),
-                    signature.into(),
+                let mut args = MultiValueEncoded::new();
+                args.push(
+                    (
+                        week,
+                        managed_biguint!(user_delegation_supply),
+                        managed_biguint!(user_lkmex_staked),
+                        signature.into(),
+                    )
+                        .into(),
                 );
+
+                sc.claim_rewards(args);
             })
     }
 
