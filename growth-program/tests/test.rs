d41663b9c246ff40ecbd94d21120cb99dedfd034
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2024-02-05 09:50:37 +0200

    test

--- growth-program/tests/test.rs
+++ growth-program/tests/test.rs
@@ -1,7 +1,9 @@
 #![allow(deprecated)]
 
 pub mod growth_program_setup;
+use growth_program::rewards::deposit::DepositRewardsModule;
 use growth_program_setup::*;
+use multiversx_sc_scenario::{managed_biguint, rust_biguint};
 
 #[test]
 fn setup_test() {
@@ -25,4 +27,45 @@ fn add_projects_test() {
     );
 
     setup.add_projects();
+}
+
+#[test]
+fn deposit_too_few_rewards_test() {
+    let mut setup = GrowthProgramSetup::new(
+        growth_program::contract_obj,
+        pair_mock::contract_obj,
+        router_mock::contract_obj,
+        simple_lock::contract_obj,
+        energy_factory::contract_obj,
+    );
+
+    setup.add_projects();
+
+    setup
+        .b_mock
+        .execute_esdt_transfer(
+            &setup.first_project_owner,
+            &setup.gp_wrapper,
+            FIRST_PROJ_TOKEN,
+            0,
+            &rust_biguint!(10),
+            |sc| {
+                sc.deposit_initial_rewards(0, 1, 27, managed_biguint!(1));
+            },
+        )
+        .assert_ok();
+}
+
+#[test]
+fn deposit_rewards_test() {
+    let mut setup = GrowthProgramSetup::new(
+        growth_program::contract_obj,
+        pair_mock::contract_obj,
+        router_mock::contract_obj,
+        simple_lock::contract_obj,
+        energy_factory::contract_obj,
+    );
+
+    setup.add_projects();
+    // setup.deposit_rewards();
 }
