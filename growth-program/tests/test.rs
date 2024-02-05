77a13332d825c4176082e3dca1648a3b0ee2d679
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2024-02-05 11:23:42 +0200

    deposit initial rewards test + fix

--- growth-program/tests/test.rs
+++ growth-program/tests/test.rs
@@ -2,11 +2,14 @@
 
 pub mod growth_program_setup;
 use growth_program::{
-    rewards::{common_rewards::CommonRewardsModule, deposit::DepositRewardsModule},
+    rewards::{
+        common_rewards::{CommonRewardsModule, RewardsInfo},
+        deposit::DepositRewardsModule,
+    },
     DEFAULT_MIN_REWARDS_PERIOD,
 };
 use growth_program_setup::*;
-use multiversx_sc_scenario::{managed_biguint, rust_biguint, DebugApi};
+use multiversx_sc_scenario::{managed_biguint, managed_token_id, rust_biguint, DebugApi};
 
 #[test]
 fn setup_test() {
@@ -110,6 +113,76 @@ fn deposit_rewards_ok_test() {
                 StaticMethods::get_first_token_full_amount_managed::<DebugApi>()
                     / DEFAULT_MIN_REWARDS_PERIOD as u32
             );
+        })
+        .assert_ok();
+}
+
+#[test]
+fn deposit_additional_rewards_ok_test() {
+    let mut setup = GrowthProgramSetup::new(
+        growth_program::contract_obj,
+        pair_mock::contract_obj,
+        router_mock::contract_obj,
+        simple_lock::contract_obj,
+        energy_factory::contract_obj,
+    );
+
+    setup.add_projects();
+    setup.deposit_rewards();
+
+    let required_balance = StaticMethods::get_first_token_full_amount();
+    setup.b_mock.set_esdt_balance(
+        &setup.first_project_owner,
+        FIRST_PROJ_TOKEN,
+        &required_balance,
+    );
+
+    setup
+        .b_mock
+        .execute_esdt_transfer(
+            &setup.first_project_owner,
+            &setup.gp_wrapper,
+            FIRST_PROJ_TOKEN,
+            0,
+            &required_balance,
+            |sc| {
+                sc.deposit_additional_rewards(1, 4, 4 + DEFAULT_MIN_REWARDS_PERIOD);
+            },
+        )
+        .assert_ok();
+
+    setup
+        .b_mock
+        .execute_query(&setup.gp_wrapper, |sc| {
+            let rewards_week_two = sc.rewards_total_amount(1, 2).get();
+            assert_eq!(
+                rewards_week_two,
+                StaticMethods::get_first_token_full_amount_managed::<DebugApi>()
+                    / DEFAULT_MIN_REWARDS_PERIOD as u32
+            );
+
+            let rewards_week_four = sc.rewards_total_amount(1, 4).get();
+            assert_eq!(
+                rewards_week_four,
+                StaticMethods::get_first_token_full_amount_managed::<DebugApi>() * 2u32
+                    / DEFAULT_MIN_REWARDS_PERIOD as u32
+            );
+
+            let rewards_week_twenty_nine = sc.rewards_total_amount(1, 29).get();
+            assert_eq!(
+                rewards_week_twenty_nine,
+                StaticMethods::get_first_token_full_amount_managed::<DebugApi>()
+                    / DEFAULT_MIN_REWARDS_PERIOD as u32
+            );
+
+            let rewards_info = sc.rewards_info(1).get();
+            let expected_rewards_info = RewardsInfo {
+                reward_token_id: managed_token_id!(FIRST_PROJ_TOKEN),
+                undistributed_rewards: managed_biguint!(0),
+                start_week: 2,
+                end_week: 30,
+            };
+            assert_eq!(rewards_info, expected_rewards_info);
         })
         .assert_ok();
 }
